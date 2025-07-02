caaf0dd0ca0b98338b9cd21456eaa2229558da05
Sabuj <asbsabuj1214@gmail.comgit config --global user.name Sabujgit commit -m 'first push'git config -global color.ui autogit config --global user.email asbsabuj1214@gmail.com>
2025-07-03 01:30:30 +0600

    reduced imports from prisma, auth

--- auth.ts
+++ auth.ts
@@ -4,19 +4,19 @@ import { prisma } from "@/db/prisma"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { compareSync } from "bcrypt-ts-edge"
 import type { NextAuthConfig } from "next-auth"
-import { CredentialsSignin } from "next-auth"
+// import { CredentialsSignin } from "next-auth"
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
-export class UserDoesNotExistError extends CredentialsSignin {
-  code = "AuthError"
-  message = "User does not exist - Please check credentials"
-}
+// export class UserDoesNotExistError extends CredentialsSignin {
+//   code = "AuthError"
+//   message = "User does not exist - Please check credentials"
+// }
 
-export class PasswordInccorectError extends CredentialsSignin {
-  code = "AuthError"
-  message = "Password is incorrect - Please check credentials"
-}
+// export class PasswordInccorectError extends CredentialsSignin {
+//   code = "AuthError"
+//   message = "Password is incorrect - Please check credentials"
+// }
 
 export const config = {
   pages: {
@@ -45,35 +45,25 @@ export const config = {
           },
         })
 
-        try {
-          if (!user) {
-            throw new UserDoesNotExistError()
-          }
-          if (user && user.password) {
-            const isMatch = compareSync(
-              credentials.password as string,
-              user.password
-            )
-            if (!isMatch) {
-              throw new PasswordInccorectError()
+        //check if user exists and password matches
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          )
+
+          //If password is correct, return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
             }
           }
-          return {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-          }
-        } catch (error) {
-          if (
-            error instanceof UserDoesNotExistError ||
-            error instanceof PasswordInccorectError
-          ) {
-            throw error
-          } else {
-            throw new Error("Unexpected error occurred during authorization")
-          }
         }
+        // if user does not exist or password doesnot match
+        return null
       },
     }),
   ],
@@ -164,3 +154,33 @@ export const config = {
 } satisfies NextAuthConfig
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
+
+// try {
+//           if (!user) {
+//             throw new UserDoesNotExistError()
+//           }
+//           if (user && user.password) {
+//             const isMatch = compareSync(
+//               credentials.password as string,
+//               user.password
+//             )
+//             if (!isMatch) {
+//               throw new PasswordInccorectError()
+//             }
+//           }
+//           return {
+//             id: user.id,
+//             name: user.name,
+//             email: user.email,
+//             role: user.role,
+//           }
+//         } catch (error) {
+//           if (
+//             error instanceof UserDoesNotExistError ||
+//             error instanceof PasswordInccorectError
+//           ) {
+//             throw error
+//           } else {
+//             throw new Error("Unexpected error occurred during authorization")
+//           }
+//         }
