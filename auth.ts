0f4d09212ef289b7488a50893da70f6b3452ce67
Sabuj <asbsabuj1214@gmail.comgit config --global user.name Sabujgit commit -m 'first push'git config -global color.ui autogit config --global user.email asbsabuj1214@gmail.com>
2025-07-04 00:07:02 +0600

    updated all packages

--- auth.ts
+++ auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/db/prisma"
-//import CredentialsProvider from "next-auth/providers/credentials"
-//import { compareSync } from "bcrypt-ts-edge"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { compareSync } from "bcrypt-ts-edge"
 import type { NextAuthConfig } from "next-auth"
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
-import authConfig from "./auth.config"
+//import authConfig from "./auth.config"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
@@ -18,7 +18,45 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     maxAge: 30 * 24 * 60 * 60,
   },
   adapter: PrismaAdapter(prisma),
-  ...authConfig,
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+
+      async authorize(credentials) {
+        if (credentials == null) return null
+
+        // check if user exists in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        })
+
+        //check if user exists and password matches
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          )
+
+          //If password is correct, return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            }
+          }
+        }
+        // if user does not exist or password doesnot match
+        return null
+      },
+    }),
+  ],
   callbacks: {
     async session({ session, user, trigger, token }: any) {
       session.user.id = token.sub
--- db/prisma.ts
+++ db/prisma.ts
@@ -1,4 +1,4 @@
-import { neonConfig } from "@neondatabase/serverless"
+import { Pool, neonConfig } from "@neondatabase/serverless"
 import { PrismaNeon } from "@prisma/adapter-neon"
 import { PrismaClient } from "@/lib/generated/prisma"
 import ws from "ws"
@@ -8,7 +8,7 @@ neonConfig.webSocketConstructor = ws
 const connectionString = `${process.env.DATABASE_URL}`
 
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-//const pool = new Pool({ connectionString })
+const pool = new Pool({ connectionString })
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 const adapter = new PrismaNeon({ connectionString })
